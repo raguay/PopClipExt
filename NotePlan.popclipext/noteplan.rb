c18a7f3414781ef61ec93d6c1b3d516302171ed9
Richard Guay <raguay@customct.com>
2017-05-20 18:18:57 +0700

    Added more options to NotePlan

--- NotePlan.popclipext/noteplan.rb
+++ NotePlan.popclipext/noteplan.rb
@@ -2,12 +2,39 @@
 
 require 'date'
 
+use_icloud = ENV['POPCLIP_OPTION_ICLOUD'].to_s == "1"
+use_mas = ENV['POPCLIP_OPTION_MAS'].to_s == "1"
 user = ENV['POPCLIP_OPTION_USER'].to_s.strip()
-todo_file_loc = "/Users/#{user}/Library/Mobile Documents/iCloud~co~noteplan~NotePlan/Documents/Calendar/" + Date.today.strftime('%Y%m%d') + ".txt"
-noteplanData = "/Users/#{user}/Library/Mobile Documents/iCloud~co~noteplan~NotePlan/Documents/Notes/"
+todo_file_loc = ""
+if use_icloud
+    todo_file_loc = "/Users/#{user}/Library/Mobile Documents/iCloud~co~noteplan~NotePlan/Documents/Calendar/" + Date.today.strftime('%Y%m%d') + ".txt"
+    noteplanData = "/Users/#{user}/Library/Mobile Documents/iCloud~co~noteplan~NotePlan/Documents/Notes/"
+else
+    if use_mas
+        todo_file_loc = "/Users/#{user}/Library/Containers/co.noteplan.NotePlan/Data/Library/Application Support/co.noteplan.NotePlan/Calendar/" + Date.today.strftime('%Y%m%d') + ".txt"
+        noteplanData = "/Users/#{user}/Library/Containers/co.noteplan.NotePlan/Data/Library/Application Support/co.noteplan.NotePlan/Notes/"
+    else
+        todo_file_loc = "/Users/#{user}/Library/Application Support/co.noteplan/Calendar/" + Date.today.strftime('%Y%m%d') + ".txt"
+        noteplanData = "/Users/#{user}/Library/Application Support/co.noteplan/Notes/"
+    end
+end
+
 input = ENV['POPCLIP_TEXT'].to_s.strip()
 journal = ENV['POPCLIP_OPTION_JOURNAL'].to_s.strip()
 modifier = ENV['POPCLIP_MODIFIER_FLAGS'].to_s
+use_star = ENV['POPCLIP_OPTION_STARDASH'].to_s == "1"
+use_bracket = ENV['POPCLIP_OPTION_BRACKET'].to_s == "1"
+
+header = ""
+if use_star
+    header = "* "
+else
+    header = "- "
+end
+
+if use_bracket
+    header = "#{header}[ ] "
+end
 
 if modifier == "0"
     #
@@ -21,7 +48,7 @@ if modifier == "0"
         todo_file = File.open("#{todo_file_loc}")
         linesInFile = IO.readlines(todo_file)
         lines = []
-        lines.push("- #{input}\n")
+        lines.push("#{header}#{input}\n")
         lines.push(linesInFile)
         IO.write(todo_file,lines.join)
         todo_file.close
@@ -34,7 +61,7 @@ if modifier == "0"
         linesInFile = IO.readlines(note_file)
         lines = []
         lines.push(linesInFile)
-        lines.push("- #{input}\n")
+        lines.push("#{header}#{input}\n")
         IO.write(note_file,lines.join)
         note_file.close
         exit(0)
@@ -52,7 +79,7 @@ else
         linesInFile = IO.readlines(note_file)
         lines = []
         lines.push(linesInFile)
-        lines.push("- #{input}\n")
+        lines.push("#{header}#{input}\n")
         IO.write(note_file,lines.join)
         note_file.close
         exit(0)
@@ -64,7 +91,7 @@ else
             todo_file = File.open("#{todo_file_loc}")
             linesInFile = IO.readlines(todo_file)
             lines = []
-            lines.push("- #{input}\n")
+            lines.push("#{header}#{input}\n")
             lines.push(linesInFile)
             IO.write(todo_file,lines.join)
             todo_file.close
